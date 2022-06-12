6955d9d22c3183d697eed07e2680a0af9914386a
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-06-12 14:51:22 -0400

    json package

--- server.js
+++ server.js
@@ -3,14 +3,23 @@
 const app = express();
 const express = require("express");
 const PORT = process.env.PORT || 3001;
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
 //parsing data array
+app.use(express.urlencoded({ extended: true }));
+
+//static file
+app.use(express.static("public"));
 
 //parsing data json
+app.use(express.json());
 
-//HTML routes
-
-//api route
+//HTML routes and api route
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 //Start listen
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}. Welcome!`);
+});
